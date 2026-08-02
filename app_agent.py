a038964c64f29572edfa6ee12c2b21defeca7550
parnia <parnianor444@gmail.com>
2026-08-02 18:52:41 +0330

    Upload data

--- app_agent.py
+++ app_agent.py
@@ -3,3 +3,16 @@ import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
 from tensorflow.keras.models import load_model
+
+API=FastAPI()
+
+model=load_model("HPLC.keras")
+with open("HPLC.keras","rb") as f:
+    Artifacts=pickle.load(f)
+scaler=Artifacts["scaler"]
+lb2=Artifacts["label_encoder_age"]
+lb3=Artifacts["label_encoder_workclass"]
+lb4=Artifacts["label_encoder_jaundice"]
+lb5=Artifacts["label_encoder_gender"]
+threshold=Artifacts["best_threshold"]
+
