9a98d57560ac79513b0cf25ba46ac6704f83c176
parnia <parnianor444@gmail.com>
2026-08-02 19:08:54 +0330

    fix name in with open

--- app_agent.py
+++ app_agent.py
@@ -8,7 +8,7 @@ from tensorflow.keras.models import load_model
 API=FastAPI()
 
 model=load_model("HPLC.keras")
-with open("HPLC.keras","rb") as f:
+with open("thalassemia_artifacts.pkl","rb") as f:
     Artifacts=pickle.load(f)
 scaler=Artifacts["scaler"]
 lb2=Artifacts["label_encoder_age"]
