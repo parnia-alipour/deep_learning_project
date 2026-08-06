be8e736d3ed92f1dabf3ac67c83eeb4da3486356
parnia <parnianor444@gmail.com>
2026-08-06 06:16:54 +0330

    del function

--- app_agent.py
+++ app_agent.py
@@ -16,29 +16,11 @@ lb4=Artifacts["label_encoder_jaundice"]
 lb5=Artifacts["label_encoder_gender"]
 threshold=Artifacts["best_threshold"]
 
-class Prediction(BaseModel):
-    Age: str
-    Gender: str
-    HbA0: float
-    HbA2: float
-    HbF: float
-    S_Window:float
-    RBC:float
-    HB:float
-    MCV:float
-    MCH: float
-    MCHC: float
-    RDWcv:float
-    Weekness: str
-    Jaundice:str
-
-
-@API.post("/predict/thalassemia")
-def predict(data:Prediction):
-    age_enc=lb2.transform([data.Age])[0]
-    gender_enc=lb5.transform([data.Gender])[0]
-    weakness_enc=lb3.transform([data.Weekness])[0]
-    jaundice_enc=lb4.transform([data.Jaundice])[0]
+def predict(Age,Gender,HbA0,HbA2,HbF,S_Window,RBC,HB,MCV,MCH,MCHC,RDWcv,Weekness,Jaundice):
+    age_enc=lb2.transform([Age])[0]
+    gender_enc=lb5.transform([Gender])[0]
+    weakness_enc=lb3.transform([Weekness])[0]
+    jaundice_enc=lb4.transform([Jaundice])[0]
 
     features=np.array([[age_enc, gender_enc, data.HbA0, data.HbA2, data.HbF,
         data.S_Window, data.RBC, data.HB, data.MCV, data.MCH,
