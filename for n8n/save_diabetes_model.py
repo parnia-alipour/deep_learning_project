f8d148a69ad2f1b660592ea553a33d38032df396
parnia <parnianor444@gmail.com>
2026-08-14 16:02:39 +0330

    save

--- for n8n/save_diabetes_model.py
+++ for n8n/save_diabetes_model.py
@@ -16,3 +16,7 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=3)
 
 model=DecisionTreeClassifier(criterion='entropy',random_state=42,class_weight='balanced',max_depth=4)
 model.fit(x_train,y_train)
+
+with open('diabetes_model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+print('diabetes_model.pkl')
