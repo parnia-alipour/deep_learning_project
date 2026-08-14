a37d36ca483e2b589a79369e40ce4f3f73e9dc04
parnia <parnianor444@gmail.com>
2026-08-14 15:53:01 +0330

    add Columns

--- for n8n/save_diabetes_model.py
+++ for n8n/save_diabetes_model.py
@@ -6,3 +6,5 @@ from sklearn.tree import DecisionTreeClassifier
 
 
 file=pd.read_csv('diabetes.csv')
+x=file[['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
+          'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']]
