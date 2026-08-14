15e6f7f05a3a6c9aab959fab78fcd9590232fea2
parnia <parnianor444@gmail.com>
2026-08-14 15:04:31 +0330

    Fixing a writing mistake

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def predict_thalassemia(data: ThalassemiaInput):
     try:
         age_enc=thal_le_age.transform([data.Age])[0]
         gender_enc=thal_le_gender.transform([data.Gender])[0]
-        wekness_enc=thal_le_weakness.transform([data.Weekness])[0]
+        weakness_enc=thal_le_weakness.transform([data.Weekness])[0]
         jaundice_enc=thal_le_jaundice.transform([data.Jaundice])[0]
     except ValueError as e:
         raise HTTPException(status_code=400, detail=f"Invalid value: {e}")
