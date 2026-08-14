e82b5b0cf291c290efc1c5e242208cced226ffde
parnia <parnianor444@gmail.com>
2026-08-14 16:01:08 +0330

    Add model and model fit

--- for n8n/save_diabetes_model.py
+++ for n8n/save_diabetes_model.py
@@ -13,3 +13,6 @@ y=file['Outcome'].values
 x=np.asanyarray(x)
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=3)
+
+model=DecisionTreeClassifier(criterion='entropy',random_state=42,class_weight='balanced',max_depth=4)
+model.fit(x_train,y_train)
